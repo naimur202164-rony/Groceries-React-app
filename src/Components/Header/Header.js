ef943210ce695c06f4d4bb9694132734bc9dad12
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-11 08:48:18 +0545

    logo added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../../assets/g logo.png";
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <nav>
+        <div>
+          <img src={logo} alt="Image-logos" />
+        </div>
+        <div></div>
+        <div></div>
+      </nav>
+    </div>
+  );
 }
